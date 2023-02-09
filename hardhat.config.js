a0c864023ec5a5076db1312dd22ce5a6bad09fd5
DramaCrypto <51626000+DramaCrypto@users.noreply.github.com>
2023-02-09 11:17:55 -0300

    Feat: add v2 extension interface and implementation

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,12 @@
 require('dotenv').config()
 require('@nomiclabs/hardhat-ethers')
 require("@nomiclabs/hardhat-etherscan");
-require('@openzeppelin/hardhat-upgrades');
 
 module.exports = {
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_NODE_URL,
-      accounts: [process.env.ROPSTEN_PRIVATE_KEY]
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_NODE_URL,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY]
+    goerli: {
+      url: process.env.GOERLI_NODE_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
     }
   },
   solidity: {
