68f0fb6ff95ef5543f00b47cc11709af61269531
DramaCrypto <51626000+DramaCrypto@users.noreply.github.com>
2023-02-09 11:17:55 -0300

    Feat: add v2 extension interface and implementation

---
+++ contracts/v2/plugins/interfaces/ISlashNftSplitPlugin.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ISlashCustomPlugin.sol";
+
+interface ISlashNftSplitPlugin is ISlashCustomPlugin {
+    /**
+     * @dev Contract upgradeable initializer
+     */
+    function initialize(address operator) external;
+
+    /**
+     * @dev part of Ownable
+     */
+    function transferOwnership(address) external;
+}
